ba7e364abd2e2d2d99045dc11603cc7b5d01dce6
the coder <bettdonald01@gmail.com>
2025-11-25 00:31:38 +0300

    Creating folder structure.

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      height: "100vh", 
+      flexDirection: "column"
+    }}>
+      <h1>Welcome to My Next.js App</h1>
+      <p>This is the homepage. Feel free to explore!</p>
+    </div>
+  );
+}
