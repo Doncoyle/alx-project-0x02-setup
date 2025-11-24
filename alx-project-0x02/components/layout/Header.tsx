a0640db5f6e2f8084e91c1842623fcd038fb172e
the coder <bettdonald01@gmail.com>
2025-11-25 00:58:05 +0300

    home

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4">
       <h2 className="text-xl font-bold">ALX Project Header</h2>
+
+       <nav className="flex gap-4">
+        <Link href="/">Home</Link>
+        <Link href="/home">/home</Link>
+        <Link href="/about">/about</Link>
+      </nav>
     </header>
   );
 };
